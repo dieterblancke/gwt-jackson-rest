39cd3b552ca556f9ad16535ecdb074f2b9bda7c2
Dieter Blancke <dieter.blancke@outlook.com>
2022-07-20 09:18:39 +0200

    added default exception mappings

--- api/src/main/java/com/github/nmorel/gwtjackson/rest/api/RestException.java
+++ api/src/main/java/com/github/nmorel/gwtjackson/rest/api/RestException.java
@@ -28,4 +28,102 @@ public class RestException extends RuntimeException {
     public String getResponse() {
         return response;
     }
+
+
+
+    public static class GatewayTimeout extends RestException {
+        public GatewayTimeout(String statusText, String response) {
+            super(504, statusText, response);
+        }
+    }
+
+    public static class ServiceUnavailable extends RestException {
+        public ServiceUnavailable(String statusText, String response) {
+            super(503, statusText, response);
+        }
+    }
+
+    public static class BadGateway extends RestException {
+        public BadGateway(String statusText, String response) {
+            super(502, statusText, response);
+        }
+    }
+
+    public static class NotImplemented extends RestException {
+        public NotImplemented(String statusText, String response) {
+            super(501, statusText, response);
+        }
+    }
+
+    public static class InternalServerError extends RestException {
+        public InternalServerError(String statusText, String response) {
+            super(500, statusText, response);
+        }
+    }
+
+    public static class UnprocessableEntity extends RestException {
+        public UnprocessableEntity(String statusText, String response) {
+            super(422, statusText, response);
+        }
+    }
+
+    public static class TooManyRequests extends RestException {
+        public TooManyRequests(String statusText, String response) {
+            super(429, statusText, response);
+        }
+    }
+
+    public static class UnsupportedMediaType extends RestException {
+        public UnsupportedMediaType(String statusText, String response) {
+            super(415, statusText, response);
+        }
+    }
+
+    public static class Gone extends RestException {
+        public Gone(String statusText, String response) {
+            super(410, statusText, response);
+        }
+    }
+
+    public static class Conflict extends RestException {
+        public Conflict(String statusText, String response) {
+            super(409, statusText, response);
+        }
+    }
+
+    public static class NotAcceptable extends RestException {
+        public NotAcceptable(String statusText, String response) {
+            super(406, statusText, response);
+        }
+    }
+
+    public static class MethodNotAllowed extends RestException {
+        public MethodNotAllowed(String statusText, String response) {
+            super(405, statusText, response);
+        }
+    }
+
+    public static class NotFound extends RestException {
+        public NotFound(String statusText, String response) {
+            super(404, statusText, response);
+        }
+    }
+
+    public static class Forbidden extends RestException {
+        public Forbidden(String statusText, String response) {
+            super(403, statusText, response);
+        }
+    }
+
+    public static class Unauthorized extends RestException {
+        public Unauthorized(String statusText, String response) {
+            super(401, statusText, response);
+        }
+    }
+
+    public static class BadRequest extends RestException {
+        public BadRequest(String statusText, String response) {
+            super(400, statusText, response);
+        }
+    }
 }
--- api/src/main/java/com/github/nmorel/gwtjackson/rest/api/RestExceptionTranslator.java
+++ api/src/main/java/com/github/nmorel/gwtjackson/rest/api/RestExceptionTranslator.java
@@ -1,11 +1,33 @@
 package com.github.nmorel.gwtjackson.rest.api;
 
+import com.github.nmorel.gwtjackson.rest.api.RestException.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class RestExceptionTranslator {
 
     private static final Map<Integer, RestExceptionSupplier<? extends RestException>> EXCEPTION_MAPPINGS = new HashMap<>();
+
+    static {
+        addStatusException(400, BadRequest::new);
+        addStatusException(401, Unauthorized::new);
+        addStatusException(403, Forbidden::new);
+        addStatusException(404, NotFound::new);
+        addStatusException(405, MethodNotAllowed::new);
+        addStatusException(406, NotAcceptable::new);
+        addStatusException(409, Conflict::new);
+        addStatusException(410, Gone::new);
+        addStatusException(415, UnsupportedMediaType::new);
+        addStatusException(422, UnprocessableEntity::new);
+        addStatusException(429, TooManyRequests::new);
+        addStatusException(415, UnsupportedMediaType::new);
+        addStatusException(500, InternalServerError::new);
+        addStatusException(501, NotImplemented::new);
+        addStatusException(502, BadGateway::new);
+        addStatusException(503, ServiceUnavailable::new);
+        addStatusException(504, GatewayTimeout::new);
+    }
 
     private RestExceptionTranslator() {
     }
@@ -16,7 +38,7 @@ public class RestExceptionTranslator {
 
     public static RestException createStatusException(int statusCode, String statusText, String responseText) {
         if (EXCEPTION_MAPPINGS.containsKey(statusCode)) {
-            return EXCEPTION_MAPPINGS.get(statusCode).get(statusCode, statusText, responseText);
+            return EXCEPTION_MAPPINGS.get(statusCode).get(statusText, responseText);
         }
 
         return new RestException(statusCode, statusText, responseText);
@@ -24,7 +46,7 @@ public class RestExceptionTranslator {
 
     public interface RestExceptionSupplier<T extends RestException> {
 
-        T get(int statusCode, String statusText, String responseText);
+        T get(String statusText, String responseText);
 
     }
 }
